80c978428b95cbce46f1d9a884372e55c5546fc0
Tox- <gau.vui97@hotmail.com>
2023-04-28 11:34:37 +0200

    Initial commit

---
+++ doctordb/src/main/java/com/mediscreen/doctordb/DoctordbApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.doctordb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctordbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoctordbApplication.class, args);
+    }
+
+}
---
+++ doctordb/src/test/java/com/mediscreen/doctordb/DoctordbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.doctordb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctordbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

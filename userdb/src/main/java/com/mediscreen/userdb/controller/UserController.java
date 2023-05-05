0c7187d95a87eb33ce577a54e3aafe4926dca9a9
Tox- <gau.vui97@hotmail.com>
2023-05-05 15:51:40 +0200

    add User API and User service

--- userdb/src/main/java/com/mediscreen/userdb/controller/UserController.java
+++ userdb/src/main/java/com/mediscreen/userdb/controller/UserController.java
@@ -1,2 +1,25 @@
-package com.mediscreen.userdb.controller;public class UserController {
+package com.mediscreen.userdb.controller;
+
+import com.mediscreen.userdb.model.User;
+import com.mediscreen.userdb.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> addUser(@Valid User user) {
+        return ResponseEntity.ok(userService.addUser(user));
+    }
 }
--- userdb/src/main/java/com/mediscreen/userdb/service/UserService.java
+++ userdb/src/main/java/com/mediscreen/userdb/service/UserService.java
@@ -1,2 +1,29 @@
-package com.mediscreen.userdb.service;public class UserService {
+package com.mediscreen.userdb.service;
+
+import com.mediscreen.userdb.exception.UserNotFoundException;
+import com.mediscreen.userdb.model.User;
+import com.mediscreen.userdb.repository.UserRepository;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @SneakyThrows
+    public User getUser(int id) {
+        var optUser = userRepository.findById(id);
+        if (optUser.isEmpty()) throw new UserNotFoundException("User with id:"+ id + " not found");
+        return optUser.get();
+    }
+
+    public User addUser(User user) {
+        return userRepository.saveAndFlush(user);
+    }
 }

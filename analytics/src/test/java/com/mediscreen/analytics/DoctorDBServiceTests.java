19162f24767e66e5de2d929db33cda5d8271a92e
Tox- <gau.vui97@hotmail.com>
2023-07-27 14:52:58 +0200

    Add unit tests for DoctorDBService

---
+++ analytics/src/test/java/com/mediscreen/analytics/DoctorDBServiceTests.java
@@ -0,0 +1,48 @@
+package com.mediscreen.analytics;
+
+import com.mediscreen.analytics.model.Data;
+import com.mediscreen.analytics.service.DoctorDBService;
+import com.mediscreen.analytics.service.WebRequestService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class DoctorDBServiceTests {
+
+    @Mock
+    WebRequestService webRequestService;
+
+    @InjectMocks
+    DoctorDBService doctorDBService;
+
+    @Test
+    void getPatientDataByIdTest() {
+        Data d = new Data();
+        d.put(Data.HIST_KEY, new ArrayList<String>());
+        when(webRequestService.doGetRequest(anyString(), any(Class.class)))
+                .thenReturn(Optional.of(d));
+
+        List<String> h = doctorDBService.getHistoryById(1);
+        Assertions.assertNotNull(h);
+    }
+
+    @Test
+    void getPatientDataByIdNotExistTest() {
+        when(webRequestService.doGetRequest(anyString(), any(Class.class)))
+                .thenReturn(Optional.empty());
+
+        List<String> h = doctorDBService.getHistoryById(1);
+        Assertions.assertNotNull(h);
+    }
+}
--- mediscreen-ui/src/main/java/com/mediscreen/ui/model/Data.java
+++ mediscreen-ui/src/main/java/com/mediscreen/ui/model/Data.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Data extends HashMap<String, Object> {
-    private static final String HIST_KEY = "patientHistory";
+    public static final String HIST_KEY = "patientHistory";
     public List<String> getHistory() {
         return (List<String>) get(HIST_KEY);
     }
